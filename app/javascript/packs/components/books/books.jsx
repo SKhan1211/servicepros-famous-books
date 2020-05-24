2c9d46af6b9826925b80e7366a1492747e16c2c7
Suhaib <suhaib92khan@gmail.com>
2020-05-24 01:00:56 -0400

    Add sorting

--- app/javascript/packs/components/books/books.jsx
+++ app/javascript/packs/components/books/books.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Book from "../../../../assets/images/placeholder_book_cover.gif";
 
 class Books extends React.Component {
@@ -13,13 +14,20 @@ class Books extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ books: this.props.books })
+    this.setState({
+      books: this.props.books,
+      location: this.props.location.search
+    });
     this.handleGhostLis();
   }
 
-  componentDidUpdate(_prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (prevState.books !== this.state.books) {
       this.handleGhostLis();
+    }
+
+    if (prevProps.path !== this.props.path) {
+      this.handleSorting(this.props.path.sort);
     }
   }
 
@@ -87,10 +95,14 @@ class Books extends React.Component {
     }
   }
 
+  removeGhostLis() {
+    let hiddenLis = document.getElementsByClassName("books__ul__list__li-hidden");
+    while (hiddenLis.length > 0) hiddenLis[0].parentNode.removeChild(hiddenLis[0]);
+  }
+
   handleSearch() {
     if (event.target.value) {
-      let hiddenLis = document.getElementsByClassName("books__ul__list__li-hidden");
-      while (hiddenLis.length > 0) hiddenLis[0].parentNode.removeChild(hiddenLis[0]);
+      this.removeGhostLis();
       let matches = [];
       this.props.books.forEach(book => {
         if (
@@ -100,8 +112,7 @@ class Books extends React.Component {
       });
       this.setState({ books: matches });
     } else {
-      let hiddenLis = document.getElementsByClassName("books__ul__list__li-hidden");
-      while (hiddenLis.length > 0) hiddenLis[0].parentNode.removeChild(hiddenLis[0]);
+      this.removeGhostLis();
       this.setState({ books: this.props.books });
     }
   }
@@ -109,16 +120,15 @@ class Books extends React.Component {
   createRatings(num) {
     // Algorithm to turn rating into Font Awesome icons
     let stars = [];
-    let filledStar = <i className="fas fa-star"></i>;
-    let unfilledStar = <i className="far fa-star"></i>;
     let numOfUnfilled = 5 - num;
 
     for (let i = 0; i < num; i++) {
-      stars.push(filledStar);
+      stars.push(<i key={i} className="fas fa-star"></i>);
+      
     };
 
     for (let i = 0; i < numOfUnfilled; i++) {
-      stars.push(unfilledStar);
+      stars.push(<i key={i + stars.length} className="far fa-star"></i>);
     }
     return (
       <div className="books__li__stars-container">
@@ -129,12 +139,60 @@ class Books extends React.Component {
     )
   }
 
+  handleSorting(path) {
+    if (path === 'alphabetical') {
+      let sortedBooks = [...this.state.books];
+      sortedBooks.sort((a, b) => this.sortHelperMethod(a, b, "alphabetical"));
+      this.removeGhostLis();
+      this.setState({ books: sortedBooks });
+      this.handleGhostLis();
+    }
+    else if (path === 'rating') {
+      let sortedBooks = [...this.state.books];
+      sortedBooks.sort((a, b) => this.sortHelperMethod(a, b, "rating")).reverse();
+      this.removeGhostLis();
+      this.setState({ books: sortedBooks });
+      this.handleGhostLis();
+    }
+    else {
+      
+    }
+  }
+
+  sortHelperMethod(a, b, type) {
+    if (type === "alphabetical") {
+      const bookTitleA = a.title[0].toUpperCase();
+      const bookTitleB = b.title[0].toUpperCase();
+
+      let comparison = 0;
+      if (bookTitleA > bookTitleB) comparison = 1;
+      else if (bookTitleA < bookTitleB) comparison = -1;
+      return comparison;
+    } else if (type === "rating") {
+      const bookRatingA = a.rating;
+      const bookRatingB = b.rating;
+
+      let comparison = 0;
+      if (bookRatingA > bookRatingB) comparison = 1;
+      else if (bookRatingA < bookRatingB) comparison = -1;
+      return comparison;
+    }
+  }
+
   render() {
-    console.log(this.state.books)
     return (
       <div className="books__outer-container">
         <header className="books__header__title-container">
           <h1>Bookstore</h1>
+
+          <div className="books__header__title__sort-container">
+            <p>Default</p>
+            <span>/</span>
+            <Link to="/books?sort=alphabetical"><p>Alphabetical</p></Link>
+            <span>/</span>
+            <Link to="/books?sort=rating"><p>Rating</p></Link>
+          </div>
+
           <div className="books__header__title__input-container">
             <input type="text" placeholder="Search all books" onChange={this.handleSearch}/>
             <i className="fas fa-search"></i>
@@ -151,7 +209,7 @@ class Books extends React.Component {
                   <p>{book.title}</p>
                   <p>by {book.author}</p>
                   <div className="books__li__info-container">
-                    <p>{this.createRatings(book.rating)}</p>
+                    {this.createRatings(book.rating)}
                     <p>${book.price}</p>
                   </div>
                 </li>
--- app/javascript/packs/components/books/books_container.js
+++ app/javascript/packs/components/books/books_container.js
@@ -1,8 +1,11 @@
 import { connect } from "react-redux";
+import queryString from "query-string";
+
 import Books from "./books";
 
-const mapStateToProps = (state, _ownProps) => ({
+const mapStateToProps = (state, ownProps) => ({
   books: state.entities.books,
+  path: queryString.parse(ownProps.location.search),
 });
 
 export default connect(mapStateToProps)(Books);
