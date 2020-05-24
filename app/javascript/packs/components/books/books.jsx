b7c6c9aa148d3590eac998d57ae8e91b69bab173
Suhaib <suhaib92khan@gmail.com>
2020-05-23 20:23:57 -0400

    Add dynamic searching algorithms

--- app/javascript/packs/components/books/books.jsx
+++ app/javascript/packs/components/books/books.jsx
@@ -9,6 +9,7 @@ class Books extends React.Component {
     };
 
     this.handleGhostLis = this.handleGhostLis.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   componentDidMount() {
@@ -19,9 +20,16 @@ class Books extends React.Component {
       .catch(error => console.log("Error while fetching books."))    
   }
 
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.books !== this.state.books) {
+      this.handleGhostLis();
+    }
+  }
+
   handleGhostLis() {
     let lis = Array.from(document.getElementsByClassName("books__ul__list-container")[0].children);
     if (lis.length % 4 !== 0) {
+      // This algorithm appends ghost li's to the document to push items to the left if there are an uneven number
       let leftover = lis.length % 4;
       if (leftover === 1) { 
         for (let i = 0; i < 3; i++) {
@@ -50,6 +58,56 @@ class Books extends React.Component {
             .appendChild(li);
         }
       }
+    } else if (lis.length < 4) {
+      // This algorithm checks if there are less than 4 li's to append any needed ghost li's so the visible li's are pushed 
+      // to the left
+      let liLength = lis.length;
+      if (liLength === 3) {
+        for (let i = 0; i < 1; i++) {
+          let li = document.createElement("li");
+          li.className = "books__ul__list__li-hidden";
+          document
+            .getElementsByClassName("books__ul__list-container")[0]
+            .appendChild(li);
+        }
+      } else if (liLength === 2) {
+        for (let i = 0; i < 2; i++) {
+          let li = document.createElement("li");
+          li.className = "books__ul__list__li-hidden";
+          document
+            .getElementsByClassName("books__ul__list-container")[0]
+            .appendChild(li);
+        }
+      } else if (liLength === 1) {
+        for (let i = 0; i < 3; i++) {
+          let li = document.createElement("li");
+          li.className = "books__ul__list__li-hidden";
+          document
+            .getElementsByClassName("books__ul__list-container")[0]
+            .appendChild(li);
+        }
+      }
+    }
+  }
+
+  handleSearch() {
+    if (event.target.value) {
+      let matches = [];
+      this.state.books.forEach(book => {
+        if (
+          book.title.toLowerCase().includes(event.target.value.toLowerCase()) ||
+          book.author.toLowerCase().includes(event.target.value.toLowerCase())
+        ) matches.push(book)
+      });
+      this.setState({ books: matches });
+    } else {
+      let hiddenLis = document.getElementsByClassName("books__ul__list__li-hidden");
+      while (hiddenLis.length > 0) hiddenLis[0].parentNode.removeChild(hiddenLis[0]);
+      // this.setState({ books: this.props.books });
+
+      fetch("https://servicepros-test-api.herokuapp.com/api/v1/books")
+        .then((res) => res.json())
+        .then((books) => this.setState({ books }));
     }
   }
 
@@ -60,7 +118,7 @@ class Books extends React.Component {
         <header className="books__header__title-container">
           <h1>Bookstore</h1>
           <div className="books__header__title__input-container">
-            <input type="text" placeholder="Search all books" />
+            <input type="text" placeholder="Search all books" onChange={this.handleSearch}/>
             <i className="fas fa-search"></i>
           </div>
         </header>
